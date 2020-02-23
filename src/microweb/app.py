27cca8f6b6a400218ae9c81e31d8b7666f38a7cc
ray <ray@stackturtle>
2020-02-23 16:41:25 +0000

    Adding basic get endpoint

--- src/microweb/app.py
+++ src/microweb/app.py
@@ -1,7 +1,33 @@
 
+from datetime import datetime
+
 from flask import Flask
+from flask import request
+from flask import jsonify
+
+from pymongo import MongoClient
+
 app = Flask(__name__)
+app.config['mongo'] = MongoClient()
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/request', methods=['POST'])
+def request_call():
+    data = request.json.copy()
+    data['timestamp'] = datetime.now().isoformat()
+
+    result = app.config['mongo'].microweb.requests.insert_one(data)
+
+    document = app.config['mongo'].microweb.requests.find_one(
+        {'_id': result.inserted_id})
+
+    return jsonify(
+        id=str(document['_id']),
+        email=document['email'],
+        title=document['title'],
+        timestamp=document['timestamp']), 201
--- src/microweb/app_test.py
+++ src/microweb/app_test.py
@@ -1,13 +1,26 @@
 
 import unittest
+from freezegun import freeze_time
 
 from microweb.app import app
+from mongomock import MongoClient
 
 
 class MicrowebTest(unittest.TestCase):
     def setUp(self):
+        app.config['mongo'] = MongoClient()
         self.client = app.test_client()
 
     def test_get(self):
         response = self.client.get('/')
         self.assertTrue('Hello, World' in response.data.decode())
+
+    @freeze_time('2019-01-01')
+    def test_request(self):
+        response = self.client.post(
+            '/request', json={
+                "email": "bob@bob.com", "title": "Bobs Burgers"})
+        self.assertEquals(201, response.status_code)
+        self.assertEquals('bob@bob.com', response.json['email'])
+        self.assertEquals('2019-01-01T00:00:00', response.json['timestamp'])
+        self.assertEquals('Bobs Burgers', response.json['title'])
