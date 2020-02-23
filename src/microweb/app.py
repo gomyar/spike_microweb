4ff94f07a1e73a187d1181670789955964c55461
ray <ray@stackturtle>
2020-02-23 19:51:11 +0000

    Extracting method get_collection

--- src/microweb/app.py
+++ src/microweb/app.py
@@ -21,19 +21,24 @@ def serialize_request(document):
     )
 
 
+def get_collection():
+    return app.config['mongo'].microweb.requests
+
+
 @app.route('/request', methods=['GET', 'POST'])
 def request_post():
-    found = app.config['mongo'].microweb.requests.find()
+    found = get_collection().find()
     if request.method == 'GET':
         results = [serialize_request(document) for document in found]
         return jsonify(results), 200
+
     elif request.method == 'POST':
         data = request.json.copy()
         data['timestamp'] = datetime.now().isoformat()
 
-        result = app.config['mongo'].microweb.requests.insert_one(data)
+        result = get_collection().insert_one(data)
 
-        document = app.config['mongo'].microweb.requests.find_one(
+        document = get_collection().find_one(
             {'_id': result.inserted_id})
 
         return jsonify(serialize_request(document)), 201
@@ -42,15 +47,16 @@ def request_post():
 @app.route('/request/<request_id>', methods=['GET', 'DELETE'])
 def request_get(request_id):
     if request.method == 'GET':
-        document = app.config['mongo'].microweb.requests.find_one(
+        document = get_collection().find_one(
             {'_id': ObjectId(request_id)})
 
         if document:
             return jsonify(serialize_request(document)), 200
         else:
             return "Not found", 404
+
     elif request.method == 'DELETE':
-        result = app.config['mongo'].microweb.requests.delete_one({
+        result = get_collection().delete_one({
             '_id': ObjectId(request_id)})
         if result.deleted_count:
             return "", 204
